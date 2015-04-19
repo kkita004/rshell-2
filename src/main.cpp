6cdbe69a7faf33502df557ef74f5728f57e781a7
Uriel Naranjo <unara001@ucr.edu>
2015-04-18 21:05:41 -0700

    Re-organized code into functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h> 
@@ -7,50 +8,55 @@
 using namespace boost;
 using namespace std;
 
-int main(int argc, char **argv){
+void run(char *command, char **flags);
+string userinput();
+void parse(const string &input, vector<string> &commands);
+
+int main(){
 	while(1){	 //continue prompting until user exits
 		vector<string> commands;
-		vector<string> flags;
-		string temp;
-		string input;
-
 		cout << "$ "; // prompt		
-		getline(cin,input);
-		
-		char_separator<char> delim(" ;");
-		tokenizer< char_separator<char> > mytok(input ,delim);
-		for(auto command = mytok.begin(); command != mytok.end() ; ++command) {
-				commands.push_back(*command);	
-				cout << *command << endl; 
-		}
-
+		string input = userinput();
+		parse(input, commands);
 		for(size_t i = 0; i < commands.size(); i++){ // loop until every command 
-													// is done or if connector && breaks
- 			for(size_t i = 0; i < flags.size(); i++){
-				cout << "FLAGS: " << flags.at(i) << endl;	
- 			}
+													// is done or if connector && breaks		
+			//run(commands.at(i), flag);
+		}	
+	}	
+	return 0;
+}
 
-			if(commands.at(i) == "exit") { // fix me
-				exit(0);
-			}
-			
+string userinput(){ // get user input 
+	string input;
+	getline(cin, input);
+	return input;
+}
+
+void parse(const string &input, vector<string> &commands){
+	char_separator<char> delim(";");
+	tokenizer< char_separator<char> > mytok(input ,delim);
+	for(auto command = mytok.begin(); command != mytok.end() ; ++command) {
+		commands.push_back(*command);	
+		cout << "tokenized command: " << *command << endl << flush; 
+	}
+
+		
+}
+void run(char *command, char **flags){
 			int	j = fork();
-	
+
 			if(j == -1){
-				perror("fork");
+				perror("Error in fork");
 			}
 				
 			else if(j == 0){	
-				if(-1 == execvp(commands.at(i).c_str(),argv)); 
-					perror(commands.at(i).c_str());
+				if(-1 == execvp(command,flags)); 
+					perror("Syntax error");
 			}
 
 			else{
 				if(-1 == wait(0))
 					perror("wait");
 			}
-		}	
-	}	
-	return 0;
 }
 
