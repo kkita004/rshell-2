9f213bcf6ff6a05cb507a8300cb6b34357a7ab14
Uriel Naranjo <unara001@ucr.edu>
2015-04-19 17:07:12 -0700

    Implemented command parameter support

--- src/main.cpp
+++ src/main.cpp
@@ -11,33 +11,35 @@
 using namespace boost;
 using namespace std;
 
-void run(char **command, char **argv);
+int run(string &command); // return -1 if command failed
 string userinput();
 void parse(const string &input, vector<string> &commands, char *del);
 void rmComments(string &s);
+//void parseCommand(vector<char*> &c, string &command);
 
 int main(int argc, char **argv){
 	while(1){	 //continue prompting until user exits
 		vector<string> commands;
+		int status = 0; // used to check if command was successful
 		cout << "$ "; // prompt		
 		string input = userinput();
-		rmComments(input);
-		cout << "Input after comments removed: " << input << endl;
+		rmComments(input); // removes comments from command string
 		char delim[] = ";";
-		parse(input, commands, delim);
+		parse(input, commands, delim); // parse string at each semicolon
 		const unsigned int size = commands.size();
-		unsigned csize = 0;
+
 		for(size_t i = 0; i < size; i++){ // loop until every command is done || exit 
 			trim(commands.at(i));
 			if(commands.at(i) == "exit") 
 				exit(0);
-			csize = commands.at(i).size();
-			char *temp = new char(csize + 1);
-			strcpy(temp, commands.at(i).c_str());
-			temp[csize] = '\0';
-			char **command = &temp;
-			run(command, argv);
-			delete [] temp;
+			status = run(commands.at(i));	
+			if(-1 == status){	
+				// code for && connector
+			}
+		//	if((-1 == status) && /* contains '||' */){
+				// code for || connector
+	//		}
+			//else keep going 
 		}	
 	}	
 	return 0;
@@ -54,27 +56,53 @@ void parse(const string &input, vector<string> &commands, char *del){
 	tokenizer< char_separator<char> > mytok(input ,delim);
 	for(auto command = mytok.begin(); command != mytok.end() ; ++command) {
 		commands.push_back(*command);
-		cout << "tokenized command: " << *command << endl << flush; 
 	}
 }
 
-void run(char **command, char **argv){
-			int	j = fork();
-			if(j == -1){
-				cout << "rshell: " << flush;
-				perror("fork");
-			}
-				
-			else if(j == 0){	
-				if(-1 == execvp(command[0] , argv)); 
-					cout << "rshell: " << flush;
-					perror(*command);
-			}
+int run(string &command){
+	int ret = 0;
+	vector<char*> c;
+	int size = command.size();
+	char *token;
+	char *temp = new char[size];
+	strcpy(temp, command.c_str());
+	token = strtok(temp, " ");
+	c.push_back(token);
+	while(token != NULL){
+		token = strtok(NULL, " ");
+		c.push_back(token);
+	}
+	c.push_back('\0');
+	char *out = c[0];
+	size_t i = 0;
+	while(out != '\0'){
+		i++;
+		out = c[i];
+	}
 
-			else{
-				if(-1 == wait(0))
-					perror("wait");
+	int j = fork();
+	if(j == -1){
+		cout << "rshell: " << flush;
+		perror("fork");
+		ret = -1;
+	}
+				
+	else if(j == 0){	
+		if(-1 == execvp(c[0],  &c[0] )){ 
+			cout << "rshell: " << flush;
+			perror(c[0]);
+			ret = -1;
+		}
+	}
+
+	else{
+		if(-1 == wait(0)){
+			perror("wait");
+			ret = -1;
 			}
+	}
+	delete []temp;
+	return ret;
 }
 
 void rmComments(string &s){
@@ -84,3 +112,25 @@ void rmComments(string &s){
 		s.erase(loc);
 	}
 }
+
+/*void parseCommand(vector<char*> &c, const string &command){
+	int size = command.size();
+	char *token;
+	char *temp = new char[size];
+	strcpy(temp, command.c_str());
+	token = strtok(temp, " ");
+	c.push_back(token);
+	while(token != NULL){
+		token = strtok(NULL, " ");
+		c.push_back(token);
+	}
+	c.push_back('\0');
+	char *out = c[0];
+	size_t i = 0;
+	while(out != '\0'){
+		cout << "PC function: " <<  &out << endl;
+		i++;
+		out = c[i];
+	}
+	delete []temp;
+}*/
